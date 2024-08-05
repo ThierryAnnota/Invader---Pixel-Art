b1a489984e5872e4ab47d42786d93eb90c930921
Thierry Annota <thierryannota@macbook-pro-de-thierry.home>
2024-08-05 15:34:19 +0200

    ajout de choix de couleur

--- app.js
+++ app.js
@@ -1,26 +1,79 @@
-const board = document.querySelector("#invader");
-const table = document.createElement("table");
-table.classList.add("table");
-board.append(table);
+const app = {
+    pixelNumber : 8,
+    colorSelected : '',
+    
+    boardSize () {
+        const formElement = document.querySelector(".configuration");
+        formElement.addEventListener("submit", handleBoardSize);
+        function handleBoardSize(event){
+            event.preventDefault();
+            const formElement = event.currentTarget;
+            console.log(formElement);
+            const gridSizeElement = document.querySelector('.gridSize');
+            console.log(gridSizeElement);
+            const gridSize = parseInt(gridSizeElement.value);
+            console.log(gridSize);
+            app.pixelNumber = gridSize;
+            app.createTable();
+            gridSizeElement.value="";
+        };
+    },
 
-const tableElement = document.querySelector(".table");
-for (let lineNumber=1 ; lineNumber<=8 ; lineNumber++) {
-    const tr = document.createElement('tr');
-    tr.classList.add('line');
-    for (let cellNumber=1 ; cellNumber<=8 ; cellNumber++) {
-        const td = document.createElement('td');
-        td.classList.add('cell');
-        td.addEventListener('click', changeColor);
-        tr.append(td);
-    };
-    tableElement.append(tr);
-};
+    createTable (){
+    
+        const board = document.querySelector("#invader");
+        board.innerHTML = '';
+        const table = document.createElement("table");
+        table.classList.add("table");
+        board.append(table);
+        
+        
+        const tableElement = document.querySelector(".table");
+        for (let lineNumber=1 ; lineNumber<=app.pixelNumber ; lineNumber++) {
+            const tr = document.createElement('tr');
+            tr.classList.add('line');
+            for (let cellNumber=1 ; cellNumber<=app.pixelNumber ; cellNumber++) {
+                const td = document.createElement('td');
+                td.classList.add('cell');
+                td.addEventListener('click', app.changeColor);
+                tr.append(td);
+            };
+            tableElement.append(tr);
+        };
+    },
 
-function changeColor(event){
-    console.log('clicky');
-    console.log(event);
-    const clickedCell = event.target;
-    clickedCell.classList.toggle('black');
+    selectColor(){
+        const colorButtons = document.querySelectorAll('.color');
+        colorButtons.forEach(button => {
+            button.addEventListener('click', app.chooseColor);
+        });
+    },
+    
+    chooseColor(event){
+        const clickedButton = event.target;
+        console.log(clickedButton.classList[1]);
+        app.colorSelected = clickedButton.classList[1];
+        console.log('dans le changeColor, après le chooseColor, colorSelected est : ' + app.colorSelected);
+    },
+    changeColor(event){     
+        const clickedCell = event.target;
+        console.log('ça clique');
+        clickedCell.style.backgroundColor = app.colorSelected;
+    },
+    
+    init (){
+        app.boardSize();
+        app.selectColor();
+        app.createTable();
+    },
 }
+
+app.init();
+
+
+
+
+
+
 
 
