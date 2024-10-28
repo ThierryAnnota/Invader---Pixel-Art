bf3ed1ae13186d134a1435b6efa3c3065859fff8
Thierry Annota <thierryannota@MBP-de-Thierry.lan>
2024-10-28 23:06:02 +0100

    feat: ajout d'html2canvas pour sauvegarder une image du pixel  + un peu de CSS

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const app = {
     pixelNumber : 8,
+    cellSize : 50,
     colorSelected : '',
+    gridCells : document.querySelector(".gridCellsNumber"),
+    cellPixel : document.querySelector(".cellPixelSize"),
     
+    saveImage () {
+    document.getElementById('saveImage').addEventListener('click', function () {
+        const artPainting = document.querySelector(".table");
+        html2canvas(artPainting, {scrollX: 0, 
+            scrollY: 0,
+            windowWidth: artPainting.scrollWidth,
+            windowHeight: artPainting.scrollHeight})
+            .then(picture => {
+                // toDataURL génère l'image. Sa qualité à 1 signifie 100% 
+            const imageToSave = picture.toDataURL("image/jpeg", 1)
+                // création d'un élément a pour simuler un téléchargement
+            const downloadLink = document.createElement("a");
+            downloadLink.href = imageToSave;
+            downloadLink.download = "pixel-art.jpeg"; 
+            downloadLink.click(); 
+        })
+    })
+    },
+
     boardSize () {
         const formElement = document.querySelector(".configuration");
         formElement.addEventListener("submit", handleBoardSize);
@@ -14,8 +36,16 @@ const app = {
             const gridSize = parseInt(gridSizeElement.value);
             console.log(gridSize);
             app.pixelNumber = gridSize;
+            app.gridCells.innerHTML = gridSize;
+            const pixelSizeElement = document.querySelector('.cellSize');
+            const pixelSizeCell = parseInt(pixelSizeElement.value);
+            app.cellSize = pixelSizeCell;
+            app.cellPixel.innerHTML = pixelSizeCell;
+            console.log(typeof(pixelSizeCell));
+            console.log(pixelSizeCell + 'dans le boardSize');
             app.createTable();
             gridSizeElement.value="";
+            pixelSizeElement.value='';
         };
     },
 
@@ -35,6 +65,9 @@ const app = {
             for (let cellNumber=1 ; cellNumber<=app.pixelNumber ; cellNumber++) {
                 const td = document.createElement('td');
                 td.classList.add('cell');
+                console.log(app.cellSize);
+                td.style.width = `${app.cellSize}px`
+                td.style.height = `${app.cellSize}px`
                 td.addEventListener('click', app.changeColor);
                 tr.append(td);
             };
@@ -65,6 +98,7 @@ const app = {
         app.boardSize();
         app.selectColor();
         app.createTable();
+        app.saveImage();
     },
 }
 
